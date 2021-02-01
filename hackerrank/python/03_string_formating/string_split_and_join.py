f8fc6b8b98173fd1fc344d2d6fc7b36fb9c390c0
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-02-01 20:16:43 +0600

    swap case and split and join string

---
+++ hackerrank/python/03_string_formating/string_split_and_join.py
@@ -0,0 +1,3 @@
+if __name__ == '__main__':
+    def split_and_join(line):
+        return "-".join(line.split())
---
+++ hackerrank/python/03_string_formating/swap_case.py
@@ -0,0 +1,3 @@
+if __name__ == '__main__':
+    def swap_case(s):
+        return s.swapcase()

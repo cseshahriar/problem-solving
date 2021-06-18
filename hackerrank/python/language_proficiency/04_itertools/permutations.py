553897ff5f072c2a189b5a9a654a1cc355cbd9ae
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-06-18 20:33:36 +0600

    combination

--- hackerrank/python/language_proficiency/04_itertools/permutations.py
+++ hackerrank/python/language_proficiency/04_itertools/permutations.py
@@ -1,7 +1,10 @@
 import itertools
 
-s,n = list(map(str,input().split(' ')))
+s,n = list(map(str,input().split(' '))) # 
 s = sorted(s)
+# s	"HACK"
+# n	"2"
+
 
 for p in list(itertools.permutations(s,int(n))):
     print(''.join(p))

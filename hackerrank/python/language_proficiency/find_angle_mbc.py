9bc3b7bf82539c481562ed1b42c49a57596b751e
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-02-18 12:16:45 +0600

    ascii problem fix

--- hackerrank/python/language_proficiency/find_angle_mbc.py
+++ hackerrank/python/language_proficiency/find_angle_mbc.py
@@ -6,14 +6,13 @@ You are given the lengths  and .
 Your task is to find  (angle , as shown in the figure) in degrees.
 """
 import math
+ab = int(input())
+bc = int(input())
 
-AB = int(input())
-BC = int(input())
+h = math.sqrt(ab**2 + bc**2)
+h = h/2.0
+adj = bc/2.0
 
-H = math.sqrt(AB**2 + BC**2)
-H = H/2.0
-adj = BC/2.0
-
-Output = int(round(math.degrees(math.acos(adj/H))))
-Output = str(Output)
-print(f'{Output}°')
+resulr = int(round(math.degrees(math.acos(adj/h))))
+degree_sign = u"\N{DEGREE SIGN}"
+print(str(resulr)+degree_sign)

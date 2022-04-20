24bc7ff97abecb2d25fa25dc9b012d11cd7ccde6
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-04-20 15:41:05 +0600

    full pyramid

--- toph/easy_problems/full_pyramid.py
+++ toph/easy_problems/full_pyramid.py
@@ -1,16 +1,12 @@
 """ full pyramid """
-N = int(input())
-k = 0
+size = int(input())
 
-for i in range(1, N+1):
-    # print((N-i) + 1) so (5-1)+1 = 5, (5-2)+1 = 4, (5-3) + 1 = 3 , 2 , 1
-    for space in range(1, (N-i) + 1):
-        print(end="  ")
+for i in range(size):
+    for j in range((size-i) - 1):
+        print(end=" ")
 
-    # print(2*i-1)
-    while k != (2*i-1):
-        print("* ", end="")  # for first loop * print after 5 space both side
-        k += 1
-
-    k = 0
+    for k in range(i+1):
+        print("*", end="")
+        if k < i:
+            print(' ', end='')
     print()

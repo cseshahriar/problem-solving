c3b21d65aa8225a3c336095b94100c8019db62c8
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-04-20 14:07:27 +0600

    big factorial

--- toph/divisors.py
+++
@@ -1,4 +0,0 @@
-A = int(input())
-for x in range(1, A+1):
-	if A % x == 0:
-		print(x)
--- toph/palindrome.py
+++
@@ -1,8 +0,0 @@
-S = input()
-case_fold = S.casefold()
-reverse_str = reversed(case_fold)
-
-if list(case_fold) == list(reverse_str):
-	print('Yes')
-else:
-	print('No')

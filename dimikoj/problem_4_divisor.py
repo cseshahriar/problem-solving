0d2ab70fcfa9693eeed7b83a6af0b2334c992153
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-12-24 16:23:22 +0600

    problem 4 divisor presentation problem

--- dimikoj/problem_4_divisor.py
+++ dimikoj/problem_4_divisor.py
@@ -1,5 +1,15 @@
 # Bismillahhir Rahmanir Rahim
 T = int(input())
+
+def find_divisors(n):
+    """ return all divisors for a n"""
+    divisors = []
+    for i in range(1, n + 1):
+        if n % i == 0:
+            divisors.append(i)
+
+    return  divisors
+
 for i in range(1, T+1):
     number = int(input())
     print(f"Case {i}: ", end="")

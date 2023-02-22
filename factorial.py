fd6af40833c7d95ed8a45008d3b63e9a5324eab5
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-02-22 12:04:57 +0600

    factorial with for loop

--- factorial.py
+++ factorial.py
@@ -3,3 +3,10 @@ import math
 n = int(input())
 result = math.factorial(n)
 print(result)
+
+# factorial with for loop
+factorial = 1
+if n >= 1:
+    for i in range(1, n+1):
+        factorial = factorial * i
+print(factorial)

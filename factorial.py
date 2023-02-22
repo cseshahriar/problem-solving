d622e30877d7a2e2c95d5f907ef6fcacd39fc925
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-02-22 12:06:55 +0600

    factorial

--- factorial.py
+++ factorial.py
@@ -6,6 +6,10 @@ print(result)
 
 # factorial with for loop
 factorial = 1
+if n < 0:
+    print("Factorial cannot be calculated, non - integer input")
+elif n == 0:
+    print("Factorial of the number is 1")
 if n >= 1:
     for i in range(1, n+1):
         factorial = factorial * i
